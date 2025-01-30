4aaac948781c4072dffd0efea8eb0daf7571e063
Krishna Kodali <krishnakodali81@gmail.com>
2025-01-30 18:38:44 -0500

    New: Add material UI

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,19 +6,26 @@ import {
   Output,
   signal,
 } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
+  imports: [MatButtonModule],
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
+  // Input as signals
   cardHeader = input<string>('Heading');
+
+  // Input as property
   @Input({ required: true }) cardDetail: string = 'Card Detail';
 
   @Output() select = new EventEmitter<number>();
 
   cardButtonValue = signal(0);
+
+  selected?: boolean = false;
 
   cardDetails = () => {
     return `${this.cardDetail} from getter`;
@@ -29,6 +36,7 @@ export class CardComponent {
   };
 
   onClickHandler2 = (): void => {
+    this.selected = true;
     this.select.emit(this.cardButtonValue());
   };
 }
